bf3c56d9ed4cd7544e46b614748cfaf921c8a128
kirilchristov <kiro.mm@gmail.com>
2019-12-03 16:53:31 -0800

    adds a bar char react feature to display times

--- client/containers/BarContainer.jsx
+++ client/containers/BarContainer.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Chart from 'react-apexcharts';
 
-const BarContainer = (props) => {
-    return ( 
-        <h2>bar container</h2>
-     );
+const cachedata = 700;
+const nocachedata = 50000;
+
+class BarContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      options : {
+        chart: {
+          background: "#f4f4f4",
+          foreColor: "#333"
+        },
+        xaxis: {
+          categories: [
+            "DB query",
+            "Cache query",
+          ]
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true
+          }
+        }
+      },
+      series: [
+        {
+          name: "Speed",
+          data: [
+            cachedata,
+            nocachedata,
+          ]
+        }
+      ],
+    }
+  }
+
+  render() {
+    return <Chart 
+      options = {this.state.options}
+      series={this.state.series}
+      type="bar"
+      height="450"
+      width="100%"
+    />
+  }
 }
- 
+
 export default BarContainer;
